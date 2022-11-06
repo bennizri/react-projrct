4da0b2bc3acbb46a3c510a91e19c1042d43f60a3
bennizri <bennizri22@gmail.com>
2022-11-06 20:34:45 +0200

    first module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
 
-function App(props) {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -22,11 +23,19 @@ function App(props) {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 export default App;
